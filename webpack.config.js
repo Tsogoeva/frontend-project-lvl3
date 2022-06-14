7e1b4b02490ceab4426a2dd2fddee96191b7cadf
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-06-14 17:46:21 +0300

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -37,7 +37,7 @@ const config = {
   },
 };
 
-module.exports = () => {
+export default () => {
   if (isProduction) {
     config.mode = 'production';
   } else {
