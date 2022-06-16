75271318eeb21036b4994390896ee0b80c950707
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-06-16 14:49:48 +0300

    add texts

---
+++ src/locales/ru.js
@@ -0,0 +1,17 @@
+export default {
+  translation: {
+    feeds: 'Фиды',
+    posts: 'Посты',
+    buttons: {
+      view: 'Просмотр',
+      close: 'Закрыть',
+      readCompletely: 'Читать полностью',
+    },
+    errors: {
+      invalidURL: 'Ссылка должна быть валидным URL',
+      rssAlreadyAdded: 'RSS уже существует',
+      invalidRSS: 'Ресурс не содержит валидный RSS',
+    },
+    success: 'RSS успешно загружен',
+  },
+};
