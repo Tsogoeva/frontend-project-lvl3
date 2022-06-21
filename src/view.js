bfef1f5f0d9626a0a126a740a876c9cc32a214e9
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-06-21 14:01:55 +0300

    a little fix

--- src/view.js
+++ src/view.js
@@ -1,17 +1,31 @@
-export default (elements, i18nInstance) => (path, value) => {
-  //   console.log(input)
-  //   console.log(path)
-  if (path === 'valid') {
-    // console.log(value)
-    switch (value) {
-      case false:
-        elements.field.classList.add('is-invalid');
-        break;
-      case true:
-        elements.field.classList.remove('is-invalid');
-        break;
-      default:
-        break;
-    }
+import onChange from 'on-change';
+import renderData from './renders.js';
+
+export default (elements, state, i18nInstance) => onChange(state, (path, value) => {
+  const currentFeedback = elements.feedback;
+  switch (value) {
+    case 'failed':
+      elements.field.classList.add('is-invalid');
+      currentFeedback.textContent = i18nInstance.t(`errors.${state.message}`);
+      currentFeedback.classList.remove('text-success');
+      currentFeedback.classList.add('text-danger');
+      break;
+
+    case 'received':
+      elements.field.classList.remove('is-invalid');
+      currentFeedback.textContent = i18nInstance.t('success');
+      currentFeedback.classList.remove('text-danger');
+      currentFeedback.classList.add('text-success');
+      elements.form.reset();
+      elements.field.focus();
+      renderData(state, elements, i18nInstance);
+      break;
+
+      /* case 'updating':
+      renderData(state, elements, i18nInstance);
+      break; */
+
+    default:
+      break;
   }
-};
+});
