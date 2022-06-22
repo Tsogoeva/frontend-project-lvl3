d278fca178bbcc2edc1965a4792f4cdd74020955
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-06-22 22:20:25 +0300

    add preview process

--- src/view.js
+++ src/view.js
@@ -1,28 +1,33 @@
 import onChange from 'on-change';
-import renderData from './renders.js';
+import { renderData, renderModal } from './renders.js';
 
-export default (elements, state, i18nInstance) => onChange(state, (path, value) => {
-  const currentFeedback = elements.feedback;
+export default (elements, state, i18nInstance) => onChange(state, (_path, value) => {
+  const { feedback, field, form } = elements;
+
   switch (value) {
     case 'failed':
-      elements.field.classList.add('is-invalid');
-      currentFeedback.textContent = i18nInstance.t(`errors.${state.message}`);
-      currentFeedback.classList.remove('text-success');
-      currentFeedback.classList.add('text-danger');
+      field.classList.add('is-invalid');
+      feedback.textContent = i18nInstance.t(`errors.${state.message}`);
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
       break;
 
     case 'received':
-      elements.field.classList.remove('is-invalid');
-      currentFeedback.textContent = i18nInstance.t('success');
-      currentFeedback.classList.remove('text-danger');
-      currentFeedback.classList.add('text-success');
-      elements.form.reset();
-      elements.field.focus();
+      field.classList.remove('is-invalid');
+      feedback.textContent = i18nInstance.t('success');
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      form.reset();
+      field.focus();
       renderData(state, elements, i18nInstance);
       break;
 
     case 'updating':
       renderData(state, elements, i18nInstance);
+      break;
+
+    case 'preview':
+      renderModal(state.previewPost, elements);
       break;
 
     default:
