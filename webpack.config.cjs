f23132ca50fb0d61162448a993ffc3c9a66feab5
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-06-23 14:38:59 +0300

    remove commits

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,13 +1,8 @@
-// const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-/*   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  }, */
   devServer: {
     open: true,
     host: 'localhost',
@@ -16,9 +11,6 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -31,20 +23,9 @@ const config = {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
         use: 'file-loader',
       },
-    // Add your rules for custom modules here
-    // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
-
-/* export default () => {
-  if (isProduction) {
-    config.mode = 'production';
-  } else {
-    config.mode = 'development';
-  }
-  return config;
-}; */
 
 module.exports = () => {
   if (isProduction) {
