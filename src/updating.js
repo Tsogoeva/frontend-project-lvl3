451ee7d24abf18e60650e4490db971f3901bb400
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-07-01 15:15:32 +0300

    add catch and small changes

--- src/updating.js
+++ src/updating.js
@@ -1,32 +1,17 @@
 import axios from 'axios';
 import _ from 'lodash';
+import { errorHandler } from './handlers.js';
 import parseRSS from './parser.js';
-import { getFeedState, getPostState } from './processing.js';
+import { getPostState } from './processing.js';
 import proxify from './proxy.js';
 
-// const updating = (view, state) => {
-const updateInterval = 5000;
+export const updateInterval = 5000;
 
-const updateRequest = (view, state, link) => axios
-  .get(link)
-  .then((response) => {
-    const data = parseRSS(response);
-    const feedState = getFeedState(state, data.feed);
-    state.currentFeedId = feedState.id;
-    state.feeds.push(feedState);
-
-    const postState = getPostState(state.currentFeedId, data.posts);
-    state.posts = [...state.posts, ...postState];
-    view.process = 'received';
-  })
-  .finally(() => setTimeout(() => updateRequest(link), updateInterval))
-  .catch(() => {});
-
-const updatePosts = (view, state) => {
+export const updatePosts = (view, state) => {
   const promises = state.feeds.map((feed) => axios.get(proxify(feed.url))
     .then((response) => {
-      const data = parseRSS(response);
-      const difference = _.differenceBy(data.posts, state.posts, 'link');
+      const parseData = parseRSS(response);
+      const difference = _.differenceBy(parseData.posts, state.posts, 'link');
       if (difference) {
         const newPostState = getPostState(feed.id, difference);
         state.posts = [...newPostState, ...state.posts];
@@ -36,7 +21,10 @@ const updatePosts = (view, state) => {
     }));
   Promise
     .all(promises)
-    .finally(() => setTimeout(updatePosts, updateInterval));
+    .catch((error) => {
+      errorHandler(error.message, state);
+      view.process = 'failed';
+      view.process = null;
+    })
+    .finally(() => setTimeout(() => updatePosts(view, state), updateInterval));
 };
-
-export default { updateInterval, updateRequest, updatePosts };
