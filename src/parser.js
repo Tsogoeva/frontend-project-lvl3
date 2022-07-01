648d8838903890e90265a90c8ee927b9a6d8e91f
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-07-01 15:14:50 +0300

    add parser error

--- src/parser.js
+++ src/parser.js
@@ -1,29 +1,30 @@
 export default (response) => {
-  try {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(response.data.contents, 'text/xml');
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(response.data.contents, 'text/xml');
 
-    const title = doc.querySelector('title').textContent;
-    const description = doc.querySelector('description').textContent;
-    const feed = {
-      title,
-      description,
-    };
-
-    const posts = [...doc.querySelectorAll('item')]
-      .map((item) => {
-        const postTitle = item.querySelector('title').textContent;
-        const postDescription = item.querySelector('description').textContent;
-        const postLink = item.querySelector('link').textContent;
-        return {
-          title: postTitle,
-          description: postDescription,
-          link: postLink,
-        };
-      });
-
-    return { feed, posts };
-  } catch (e) {
-    throw new Error('parseError');
+  const parserError = doc.querySelector('parsererror');
+  if (parserError) {
+    throw new Error('Parser Error');
   }
+
+  const title = doc.querySelector('title').textContent;
+  const description = doc.querySelector('description').textContent;
+  const feed = {
+    title,
+    description,
+  };
+
+  const posts = [...doc.querySelectorAll('item')]
+    .map((item) => {
+      const postTitle = item.querySelector('title').textContent;
+      const postDescription = item.querySelector('description').textContent;
+      const postLink = item.querySelector('link').textContent;
+      return {
+        title: postTitle,
+        description: postDescription,
+        link: postLink,
+      };
+    });
+
+  return { feed, posts };
 };
