999668a1f697349e4c2e0d3ed5479550cb42ec5d
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-07-04 14:37:46 +0300

    add entry and output

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,27 +1,34 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  /* devServer: {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
     open: true,
     host: 'localhost',
-  }, */
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
   ],
   module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: 'url-loader?limit=10000',
+    },
+    {
+      test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+      use: 'file-loader',
+    },
     ],
   },
 };
