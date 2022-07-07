6abcaeb4c589e249ab7d8e587d789eda4ffe5e23
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-07-07 15:02:55 +0300

    fix errorHandler, handlerVisitedPosts and add state.visitedPosts

--- src/init.js
+++ src/init.js
@@ -1,9 +1,154 @@
 import i18n from 'i18next';
+import axios from 'axios';
+import { Modal } from 'bootstrap';
 import * as yup from 'yup';
+import _ from 'lodash';
 import render from './view.js';
+import parseRSS from './parser.js';
 import ru from './locales/ru.js';
-import { eventHandlers } from './handlers.js';
-import { updatePosts } from './updating.js';
+
+const proxify = (url) => {
+  const proxy = new URL('https://allorigins.hexlet.app/get');
+  proxy.searchParams.set('disableCache', 'true');
+  proxy.searchParams.set('url', url);
+  return proxy.toString();
+};
+
+const validator = (link, feeds) => {
+  const urls = feeds.map(({ url }) => url);
+  return yup.string()
+    .url()
+    .notOneOf(urls)
+    .validate(link);
+};
+
+const errorHandler = (error, state) => {
+  switch (true) {
+    case (error.isAxiosError):
+      state.message = 'networkError';
+      break;
+    case (error.isParserError):
+      state.message = 'parseError';
+      break;
+    case (error instanceof yup.ValidationError):
+      state.message = error.message;
+      break;
+    default:
+      state.message = 'unknownError';
+      break;
+  }
+};
+
+const getFeedState = (state, feed) => ({
+  title: feed.title,
+  description: feed.description,
+  id: _.uniqueId('feed_'),
+  url: state.currentURL,
+});
+
+const getPostState = (feedId, posts) => posts.map((post) => ({
+  title: post.title,
+  description: post.description,
+  link: post.link,
+  id: _.uniqueId('post_'),
+  feedId,
+}));
+
+const eventHandlers = (view, state, elements) => {
+  const modal = new Modal(elements.modal);
+
+  const {
+    form,
+    postsContainer,
+    modalClosingButtons,
+  } = elements;
+
+  const handlerForm = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    state.currentURL = formData.get('url');
+    view.process = 'receiving';
+
+    validator(state.currentURL, state.feeds)
+      .then(() => axios.get(proxify(state.currentURL)))
+      .then((response) => {
+        const data = parseRSS(response);
+        const feedState = getFeedState(state, data.feed);
+        state.currentFeedId = feedState.id;
+        state.feeds.push(feedState);
+
+        const postState = getPostState(state.currentFeedId, data.posts);
+        state.posts = [...state.posts, ...postState];
+        view.process = 'received';
+      })
+      .catch((error) => {
+        errorHandler(error, state);
+        view.process = 'failed';
+        view.process = null;
+      });
+  };
+
+  const handlerVisitedPosts = (e) => {
+    const currentId = e.target.dataset?.id;
+
+    const getPostId = (prefix, targetId) => {
+      const postId = targetId.substring(prefix.length, targetId.length);
+      const post = state.posts.find(({ id }) => id === postId);
+
+      state.visitedPosts.add(post);
+      view.process = 'visited post';
+
+      return post;
+    };
+
+    const btnIdPrefix = 'btn_';
+    const aIdPrefix = 'a_';
+
+    if (currentId && currentId.match(/btn_post_\d+$/)) {
+      const currentPost = getPostId(btnIdPrefix, currentId);
+      state.previewPost = currentPost;
+      view.process = 'preview';
+      modal.show();
+    }
+
+    if (currentId && currentId.match(/a_post_\d+$/)) {
+      getPostId(aIdPrefix, currentId);
+    }
+  };
+
+  const handlerClosePreview = () => {
+    modal.hide();
+  };
+
+  form.addEventListener('submit', handlerForm);
+  postsContainer.addEventListener('click', handlerVisitedPosts);
+  modalClosingButtons.forEach((button) => {
+    button.addEventListener('click', handlerClosePreview);
+  });
+};
+
+const updatePosts = (view, state) => {
+  const updateInterval = 5000;
+
+  const promises = state.feeds.map((feed) => axios.get(proxify(feed.url))
+    .then((response) => {
+      const parseData = parseRSS(response);
+      const difference = _.differenceBy(parseData.posts, state.posts, 'link');
+      if (difference) {
+        const newPostState = getPostState(feed.id, difference);
+        state.posts = [...newPostState, ...state.posts];
+        view.process = 'updating';
+      }
+    }));
+  Promise
+    .all(promises)
+    .catch((error) => {
+      errorHandler(error, state);
+      view.process = 'failed';
+      view.process = null;
+    })
+    .finally(() => setTimeout(() => updatePosts(view, state), updateInterval));
+};
 
 export default () => {
   const defaultLanguage = 'ru';
@@ -18,10 +163,10 @@ export default () => {
   }).then(() => {
     yup.setLocale({
       string: {
-        url: 'URL is invalid',
+        url: 'invalidURL',
       },
       mixed: {
-        notOneOf: 'RSS already added',
+        notOneOf: 'rssExist',
       },
     });
   });
@@ -42,10 +187,11 @@ export default () => {
   };
 
   const state = {
-    process: null, // receiving, received, updating, failed, preview
+    process: null, // receiving, received, updating, failed, preview, visited post
     message: '',
     currentURL: '',
     currentFeedId: 0,
+    visitedPosts: new Set(),
     previewPost: '',
     feeds: [],
     posts: [],
