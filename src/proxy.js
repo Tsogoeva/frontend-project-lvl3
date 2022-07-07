946eab5dec9bfed4c9ed2f9c2c651a303032ed2d
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-07-07 14:58:42 +0300

    merge into init.js

--- src/proxy.js
+++
@@ -1,6 +0,0 @@
-export default (url) => {
-  const proxy = new URL('https://allorigins.hexlet.app/get');
-  proxy.searchParams.set('disableCache', 'true');
-  proxy.searchParams.set('url', url);
-  return proxy.toString();
-};
